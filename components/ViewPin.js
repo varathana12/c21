605741a910601dcacec87c1e0095d05da09c8f43
va rathana <varathana15@kit.edu.kh>
2018-06-09 12:02:56 +0700

    dd

--- components/ViewPin.js
+++ components/ViewPin.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {View,Text,Image,StyleSheet} from 'react-native'
-import qrImage from '../assets/images/qr-code.png'
 
 
 
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,18 +1,11 @@
 import React from "react";
 import {View,Text,StyleSheet,Image,Platform} from 'react-native'
-//import { MapView } from 'expo';
+import { MapView } from 'expo';
 import ViewPin from '../components/ViewPin'
 import {location} from "../data";
-import Tooltip from 'rn-tooltip';
-import MakerPoint from '../components/maker'
 import pin from '../assets/images/placeholder.png'
-import MapView from 'react-native-maps';
+
 export default class MapScreen extends React.Component{
-    state ={
-        loaded:false,
-        iconLoaded:false,
-        image:null
-    }
     constructor(props){
         super(props)
     }
@@ -28,12 +21,11 @@ export default class MapScreen extends React.Component{
                      initialRegion={{
                          latitude: 11.629549102704,
                          longitude: 104.87701304512,
-                         latitudeDelta: 0.0722,
-                         longitudeDelta: 0.053,
+                         latitudeDelta: 0.1022,
+                         longitudeDelta: 0.103,
                      }}
             >
                 {
-
                     location.map((item,index)=>{
 
                         return (
